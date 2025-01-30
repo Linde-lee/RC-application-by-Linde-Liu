5577e6b97707b002895eb5854b2f3d38d869e4de
Linde-lee <lindeliu99@gmail.com>
2025-01-30 11:35:29 +0100

    Update calculator-program_lindeLiu.py

--- calculator-program_lindeLiu.py
+++ calculator-program_lindeLiu.py
@@ -15,35 +15,16 @@ operations = {
     "-": sub,
     "*": mul,
     "/": div,
-} #now my four functions are stored in this dictionary called operations
-
-#print(operations["*"](4,8))
+}
 
 def calculator ():
     n1_input = float(input("What is your first number? "))
     picked_operation = input("Pick an operation (+, -, *. /): ")
     n2_input = float(input("What is your second number? "))
-
-    #now programming the operation with the given inputs:
-    #if picked_operation == "+":
-        #result = operations["+"](n1= n1_input, n2=n2_input)
-       #print(result)
-    #elif picked_operation == "-":
-        #result = operations["-"](n1= n1_input, n2=n2_input)
-        #print(result)
-    #elif picked_operation == "*":
-        #result = operations["*"](n1= n1_input, n2=n2_input)
-        #print(result)
-    #else:
-       # result = operations["/"](n1= n1_input, n2=n2_input)
-        #print(result)
-    #this is faster: and this is why we chose to use dictionary to avoid this if-elif-else thing.to be more dynamic and cleaner
-
+    
     answer_1 = operations[picked_operation](n1= n1_input, n2=n2_input)
     print(f"{n1_input} {picked_operation} {n2_input} = {answer_1}")
 
-
-    #asking if continue or restart if/else
     second_question = input("if you want to continue working with the previous result type 'y'. If you want to restart type 'n': ")
     if second_question == "y":
         picked_operation_two = input("Pick an operation (+, -, *. /): ")
